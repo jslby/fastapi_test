6923c1c8d7500179599a963f5637fa236c7bd078
Iaroslav <anon@MacBook-Air-Iaroslav.local>
2026-03-22 17:59:01 +0300

    grouping fb creo

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from fastapi import FastAPI
 import pandas as pd
 
-fb_raw = pd.read_excel("fb_report.xlsx")
-kt_raw = pd.read_csv("kt_report.csv", sep=";")
+fb_raw = pd.read_excel("fb_report.xlsx").iloc[:, 0:4]
+fb_raw.columns = ["creative", "spend", "impressions", "clicks"]
 
-fb = fb_raw.iloc[:, 0:5]
-fb.columns = ["creative", "spend", "impressions", "clicks", "cpc"]
 
-kt = kt_raw.iloc[:, 0:4]
-kt.columns = ["creative", "regs", "deps", "revenue"]
+kt_raw = pd.read_csv("kt_report.csv", sep=";").iloc[:, 0:4]
+kt_raw.columns = ["creative", "regs", "deps", "revenue"]
 
-df = pd.merge(fb, kt, on="creative", how="left")
+fb = (
+    fb_raw
+    .groupby("creative")
+    .agg({
+        "spend":        "sum",
+        "impressions":   "sum",
+        "clicks":       "sum"
+    })
+    .reset_index()
+)
+
+
+df = pd.merge(fb, kt_raw, on="creative", how="left")
 df["regs"] = df["regs"].fillna(0).astype(int)
 df["deps"] = df["deps"].fillna(0).astype(int)
 df["clicks"] = df["clicks"].fillna(0).astype(int)
-df["cpc"] = df["cpc"].fillna(0)
 df["impressions"] = df["impressions"].fillna(0).astype(int)
 
 
 df["spend"] = df["spend"].round(2)
-df["cpc"] = df["cpc"].round(2)
 df["revenue"] = df["spend"].round(2)
 
 df.to_csv(
